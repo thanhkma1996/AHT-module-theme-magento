3ec1546524a9454345d230874b70f63f7cf4446f
Thanhkma1996 <soldiersoociu@gmail.com>
2019-01-14 11:09:35 +0700

    module custom theme magento 2

---
+++ Genmato/default/requirejs-config.js
@@ -0,0 +1,12 @@
+var config = {
+
+    paths: {
+        'owlcarousel': "Genmato_default/web/js/owl.carousel"
+
+    },
+    shim: {
+        'owlcarousel': {
+            deps: ['jquery']
+        },
+    }
+};
